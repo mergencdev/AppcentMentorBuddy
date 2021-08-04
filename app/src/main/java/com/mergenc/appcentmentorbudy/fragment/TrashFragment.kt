ed19d2e5ebb2de01125f66f1a24d1fe0ed805aa5
Mehmet Emin Ergenç <mergencdev@gmail.com>
2021-08-04 10:18:00 +0300

    first commit

---
+++ app/src/main/java/com/mergenc/appcentmentorbudy/fragment/TrashFragment.kt
@@ -0,0 +1,26 @@
+package com.mergenc.appcentmentorbudy.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mergenc.appcentmentorbudy.R
+
+class TrashFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trash, container, false)
+    }
+
+
+}
