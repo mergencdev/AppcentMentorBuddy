3eb535c74d40d8a30d0b0a5a1291451c6444c96d
Mehmet Emin Ergenç <mergencdev@gmail.com>
2021-08-04 10:50:48 +0300

    tabs set

--- app/src/main/java/com/mergenc/appcentmentorbudy/MainActivity.kt
+++ app/src/main/java/com/mergenc/appcentmentorbudy/MainActivity.kt
@@ -2,10 +2,31 @@ package com.mergenc.appcentmentorbudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mergenc.appcentmentorbudy.adapter.ViewPagerAdapter
+import com.mergenc.appcentmentorbudy.fragment.FeedFragment
+import com.mergenc.appcentmentorbudy.fragment.TrashFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTabs()
+    }
+
+    private fun setTabs() {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        adapter.addFragment(FeedFragment(), "Feed")
+        adapter.addFragment(TrashFragment(), "Trash")
+
+        viewPager.adapter = adapter
+
+        // give tab settings with viewpager;
+        tabs.setupWithViewPager(viewPager)
+        // give tab icon with setIcon by its index;
+        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_dynamic_feed_24)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_delete_24)
     }
 }
